d3b22cbf8096438c9977915cd17dc0211d80c622
Kondrat Shmoylov <kondrat.shmoylov@gmail.com>
2018-03-31 11:49:51 +0200

    integrate prettier

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-import App from './App'
+import App from "./App";
 
-const app = new App().express
+const app = new App().express;
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
-app.listen(port, (err) => {
-  if (err) {
-    return console.log(err)
-  }
-  return console.log(`server is listening on http://localhost:${port}`)
-})
+app.listen(port, err => {
+    if (err) {
+        return console.log(err);
+    }
+    return console.log(`server is listening on http://localhost:${port}`);
+});
